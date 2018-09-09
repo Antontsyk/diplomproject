c83f2352fb1ff8c9a75e42f31d2dbb3068a3ca35
Mag <antontsyk@gmail.com>
2018-09-09 14:55:16 +0300

    hide login pass mail

--- testMailSMTP.js
+++ testMailSMTP.js
@@ -7,8 +7,8 @@ var transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'startupmap@we.guru',
-        pass: 'startupmap2007'
+        user: '',
+        pass: ''
     },
     tls: {
         // do not fail on invalid certs
@@ -17,7 +17,7 @@ var transporter = nodemailer.createTransport({
     }
 });
 var mailOptions = {
-    from: 'startupmap@we.guru',
+    from: '',
     to: 'Antontsyk@gmail.com',
     subject: 'Account Verification Token',
     text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/'
